e9aaa1bce3892d8233824e9d19856b877cc8a7e5
Nikita Denisov <nikita160412@yandex.ru>
2021-03-19 22:18:44 +0700

    This is infinitly work

--- my_zmq/main.cpp
+++ my_zmq/main.cpp
@@ -4,8 +4,14 @@
 int main()
 {
     zmq::context_t ctx;
-    zmq::socket_t sock(ctx, zmq::socket_type::push);
-    sock.bind("inproc://test");
-    sock.send(zmq::str_buffer("Hello, world"), zmq::send_flags::dontwait);
-    std::cerr << "wtf\n";
+    zmq::socket_t sock_send(ctx, zmq::socket_type::push);
+    const std::string address = "tcp://127.0.0.1:5000";
+    sock_send.bind(address);
+    sock_send.send(zmq::str_buffer("Hello, world"), zmq::send_flags::dontwait);
+
+    zmq::socket_t sock_recv(ctx, zmq::socket_type::pull);
+    sock_recv.connect(address);
+    zmq::message_t msg;
+    sock_recv.recv(msg, zmq::recv_flags::none);
+    std::cout << msg.to_string_view() << std::endl;
 }
