abb6cb9775880075bfaa4ddc188d22a5c98cbf6a
Nikita Denisov <nikita160412@yandex.ru>
2021-03-19 23:50:21 +0700

    Try to make chat

--- my_zmq/client.cpp
+++ my_zmq/client.cpp
@@ -1,32 +1,28 @@
-//
-//  Hello World client in C++
-//  Connects REQ socket to tcp://localhost:5555
-//  Sends "Hello" to server, expects "World" back
-//
 #include <zmq.hpp>
 #include <string>
 #include <iostream>
 
-int main ()
-{
-    //  Prepare our context and socket
+int main () {
     zmq::context_t context (1);
     zmq::socket_t socket (context, ZMQ_REQ);
 
-    std::cout << "Connecting to hello world server..." << std::endl;
+    std::cout << "Connecting to char server..." << std::endl;
     socket.connect ("tcp://localhost:5555");
 
-    //  Do 10 requests, waiting each time for a response
-    for (int request_nbr = 0; request_nbr != 10; request_nbr++) {
-        zmq::message_t request (5);
-        memcpy (request.data (), "Hello", 5);
-        std::cout << "Sending Hello " << request_nbr << "..." << std::endl;
-        socket.send (request);
+    while (true) {
+        std::cout << "> ";
+        std::cout.flush();
+        std::string message;
+        std::cin >> message;
+        zmq::message_t msg(message);
+
+        std::cout << "Sending message ..." << std::endl;
+        socket.send (msg);
+        std::cout << "Message is sent" << std::endl;
 
         //  Get the reply.
         zmq::message_t reply;
-        socket.recv (&reply);
-        std::cout << "Received World " << request_nbr << std::endl;
+        socket.recv (reply);
+        std::cout << "Received:  " << reply.to_string_view() << std::endl;
     }
-    return 0;
 }
--- my_zmq/server.cpp
+++ my_zmq/server.cpp
@@ -1,21 +1,9 @@
-//
-//  Hello World server in C++
-//  Binds REP socket to tcp://*:5555
-//  Expects "Hello" from client, replies with "World"
-//
 #include <zmq.hpp>
 #include <string>
 #include <iostream>
-#ifndef _WIN32
 #include <unistd.h>
-#else
-#include <windows.h>
-
-#define sleep(n)	Sleep(n)
-#endif
 
 int main () {
-    //  Prepare our context and socket
     zmq::context_t context (1);
     zmq::socket_t socket (context, ZMQ_REP);
     socket.bind ("tcp://*:5555");
@@ -24,16 +12,15 @@ int main () {
         zmq::message_t request;
 
         //  Wait for next request from client
-        socket.recv (&request);
-        std::cout << "Received Hello" << std::endl;
+        socket.recv(request);
+        std::cout << "Received " << request.to_string_view() << std::endl;
 
         //  Do some 'work'
         sleep(1);
 
         //  Send reply back to client
-        zmq::message_t reply (5);
-        memcpy (reply.data (), "World", 5);
-        socket.send (reply);
+        zmq::message_t msg(std::move(request));
+        socket.send(msg);
     }
     return 0;
 }
