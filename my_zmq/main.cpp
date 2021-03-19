2d65c3353ac5a3a863c20097f1ec53d487cf417e
Nikita Denisov <nikita160412@yandex.ru>
2021-03-19 22:42:11 +0700

    This is the same

--- my_zmq/main.cpp
+++ my_zmq/main.cpp
@@ -5,12 +5,14 @@ int main()
 {
     zmq::context_t ctx;
     zmq::socket_t sock_send(ctx, zmq::socket_type::push);
+    zmq::socket_t sock_recv(ctx, zmq::socket_type::pull);
+
     const std::string address = "tcp://127.0.0.1:5000";
     sock_send.bind(address);
+    sock_recv.connect(address);
+
     sock_send.send(zmq::str_buffer("Hello, world"), zmq::send_flags::dontwait);
 
-    zmq::socket_t sock_recv(ctx, zmq::socket_type::pull);
-    sock_recv.connect(address);
     zmq::message_t msg;
     sock_recv.recv(msg, zmq::recv_flags::none);
     std::cout << msg.to_string_view() << std::endl;
