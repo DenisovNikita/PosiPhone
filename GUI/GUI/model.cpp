4f3495bd162371be05e3992e8aac03bb5233d160
Alexey Pozdnyakov <pas.9250306384@gmail.com>
2021-04-12 01:09:41 +0300

    delete unnecessary files

--- GUI/GUI/model.cpp
+++
@@ -1,9 +0,0 @@
-#include "model.h"
-
-model::model() : me(MyCircle::create(QPointF(100, 100))) { // TODO: where to spawn
-    this->setSceneRect(-300, -300, 600, 600);
-    this->setItemIndexMethod(QGraphicsScene::NoIndex);
-
-    // TODO: pull other circles
-    this->addItem(me);
-}
