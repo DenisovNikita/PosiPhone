abba9581726a8e082c228284a6787a746b8efef6
Nikita Denisov <nikita160412@yandex.ru>
2021-05-05 15:10:03 +0300

    Something, that can be chat

--- my_zmq/client.cpp
+++ my_zmq/client.cpp
@@ -1,28 +1,112 @@
-#include <zmq.hpp>
-#include <string>
-#include <iostream>
+// Install-Package boost -Version 1.55.0.16
+// Install-Package boost_system-vc110 -Version 1.55.0.16
 
-int main () {
+#include <zmq.hpp>
+#include <sstream>
+#include <iostream>
+#include <thread>
+#include <map>
+
+#include <boost/serialization/serialization.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+using std::string;
+using std::stringstream;
+using std::cout, std::cin;
+
+struct Message {
+    string from;
+    string to;
+    string message;
+
+    template<class Archive>
+    void serialize(Archive & ar, const unsigned int version)
+    {
+        ar & from;
+        ar & to;
+        ar & message;
+    }
+};
+
+
+void send(zmq::socket_t & socket, Message message) {
+    stringstream stream;
+    boost::archive::text_oarchive serializer(stream);
+    serializer << message;
+    auto serialized = stream.str();
+
+    zmq::message_t request (serialized.size());
+    memcpy (request.data (), serialized.data(), serialized.size());
+    socket.send (request);
+}
+
+Message receive(zmq::socket_t & socket) {
+    Message requestMessage;
+
+    zmq::message_t zmqRequestMessage;
+    socket.recv (&zmqRequestMessage);
+    string data( static_cast<char*>(zmqRequestMessage.data()), zmqRequestMessage.size());
+    stringstream inputStream(data);
+    boost::archive::text_iarchive deserializer(inputStream);
+    deserializer >> requestMessage;
+
+    return requestMessage;
+}
+
+int main ()
+{
+    auto serverName = "tcp://localhost:5555";
+
     zmq::context_t context (1);
+
     zmq::socket_t socket (context, ZMQ_REQ);
 
-    std::cout << "Connecting to char server..." << std::endl;
-    socket.connect ("tcp://localhost:5555");
+    cout << "Connecting to hello world server…" << "\n";
+    socket.connect (serverName);
 
-    while (true) {
-        std::cout << "> ";
-        std::cout.flush();
-        std::string message;
-        std::cin >> message;
-        zmq::message_t msg(message);
+    Message m;
+    cout << "name: ";
+    cin >> m.from;
 
-        std::cout << "Sending message ..." << std::endl;
-        socket.send (msg);
-        std::cout << "Message is sent" << std::endl;
+    auto send_and_receive = [&]() {
+        send(socket, m);
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        return receive(socket);
+    };
 
-        //  Get the reply.
-        zmq::message_t reply;
-        socket.recv (reply);
-        std::cout << "Received:  " << reply.to_string_view() << std::endl;
+    while(true) {
+        cout << "> ";
+        string command;
+        cin >> command;
+        if (command == "help") {
+            cout << "Send message:              [send] [to] [message]\n";
+            cout << "View next message in chat: [get]\n";
+            cout << "View participants:         [list]\n";
+            continue;
+        } else if (command == "send") {
+            cin >> m.to >> m.message;
+            auto result = send_and_receive();
+            cout << "result: from=" << result.from << ", to=" << result.to
+                 << ", message=" << result.message << "\n";
+        } else if (command == "get") {
+            m.to = "Server";
+            m.message = "get";
+            auto result = send_and_receive();
+            if(result.from != "Server") {
+                cout << result.from << ": "
+                     << result.message << "\n";
+            }
+        } else if (command == "list") {
+            m.to = "Server";
+            m.message = "list";
+            auto result = send_and_receive();
+            assert(result.from == "Server");
+            cout << "Participants, who send something\n";
+            cout << result.message;
+        } else {
+            assert(false);
+        }
     }
 }
--- my_zmq/server.cpp
+++ my_zmq/server.cpp
@@ -1,26 +1,102 @@
-#include <zmq.hpp>
-#include <string>
 #include <iostream>
-#include <unistd.h>
+#include <sstream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
 
-int main () {
+#include <boost/serialization/serialization.hpp>
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/archive/text_iarchive.hpp>
+
+#include <zmq.hpp>
+#include <boost/archive/text_oarchive.hpp>
+
+using std::string;
+using std::stringstream;
+using std::cout, std::cin;
+
+struct Message {
+    string from;
+    string to;
+    string message;
+
+    template<class Archive>
+    void serialize(Archive & ar, const unsigned int version)
+    {
+        ar & from;
+        ar & to;
+        ar & message;
+    }
+};
+
+std::vector<Message> queue;
+std::unordered_set<string> participants;
+
+int main() {
     zmq::context_t context (1);
     zmq::socket_t socket (context, ZMQ_REP);
     socket.bind ("tcp://*:5555");
 
     while (true) {
-        zmq::message_t request;
+        try {
+            Message requestMessage;
 
-        //  Wait for next request from client
-        socket.recv(request);
-        std::cout << "Received " << request.to_string_view() << std::endl;
+            zmq::message_t zmqRequestMessage;
+            socket.recv (&zmqRequestMessage);
+            string data( static_cast<char*>(zmqRequestMessage.data()), zmqRequestMessage.size());
+            stringstream inputStream(data);
+            boost::archive::text_iarchive deserializer(inputStream);
+            deserializer >> requestMessage;
 
-        //  Do some 'work'
-        sleep(1);
+            participants.insert(requestMessage.from);
 
-        //  Send reply back to client
-        zmq::message_t msg(std::move(request));
-        socket.send(msg);
+            Message responseMessage;
+            responseMessage.from = "Server";
+            responseMessage.to = requestMessage.from;
+            responseMessage.message = "OK";
+
+            if (requestMessage.to == "Server") {
+                if (requestMessage.message == "get") {
+                    auto it = find_if(queue.begin(), queue.end(),
+                                      [&](Message &m) {
+                                          return m.to == requestMessage.from;
+                                      });
+
+                    if (it != queue.end()) {
+                        responseMessage = *it;
+                        queue.erase(it);
+                    }
+                } else if (requestMessage.message == "list") {
+                    responseMessage.message.clear();
+                    for (const auto &p : participants) {
+                        responseMessage.message += p + "\n";
+                    }
+                } else {
+                    assert(false);
+                }
+            } else {
+                cout << "Received Hello "
+                     << requestMessage.from << " -> "
+                     << requestMessage.to << ": "<< requestMessage.message << "\n";
+
+
+                queue.push_back(requestMessage);
+            }
+
+
+            stringstream outputStream;
+            boost::archive::text_oarchive serializer(outputStream);
+            serializer << responseMessage;
+            auto serialized = outputStream.str();
+
+            zmq::message_t response (serialized.size());
+            memcpy (response.data (), serialized.data(), serialized.size());
+            socket.send (response);
+
+        } catch (const std::exception &e) {
+            cout << e.what() << "\n";
+        }
     }
-    return 0;
 }
