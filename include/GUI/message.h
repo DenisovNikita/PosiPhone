d5af0b8e45ffc446671af057b95e30b9ac4ced66
Alexey Pozdnyakov <pas.9250306384@gmail.com>
2021-05-20 02:53:39 +0300

    improved message class with bugs

--- include/GUI/message.h
+++ include/GUI/message.h
@@ -5,18 +5,62 @@
 #include <string>
 #include <utility>
 
-struct Message {
-    enum Message_type { Create, Destroy, Move, Audio } type;
-    const std::int64_t id;
-    const std::string name;
-    const double x;
-    const double y;
+class Message {
+public:
+    enum MessageType {
+        Create,
+        Destroy,
+        Move,
+        AudioSource,
+        AudioResult,
+        CheckName
+    } type_;
 
-    Message(Message_type type,
+private:
+    const std::int64_t id_;
+    const std::string name_;
+    const double x_;
+    const double y_;
+
+    Message(MessageType type,
             std::int64_t id,
             std::string name,
             double x,
             double y);
+    static Message create_by_id(MessageType type, std::int64_t id);
+    static Message create_by_id_x_y(MessageType type,
+                                    std::int64_t id,
+                                    double x,
+                                    double y);
+    static Message create_by_id_name(MessageType type,
+                                     std::int64_t id,
+                                     std::string name);
+    static Message create_by_id_name_x_y(MessageType type,
+                                         std::int64_t id,
+                                         std::string name,
+                                         double x,
+                                         double y);
+
+public:
+    [[nodiscard]] MessageType type() const;
+    [[nodiscard]] std::int64_t id() const;
+    [[nodiscard]] std::string name() const;
+    [[nodiscard]] double x() const;
+    [[nodiscard]] double y() const;
+    template <typename... Args>
+    static Message create(MessageType&& type, Args&&... args) {
+        if (type == MessageType::Create) {
+            return create_by_id_name_x_y(type, std::forward<Args>(args)...);
+        } else if (type == MessageType::Destroy ||
+                   type == MessageType::AudioResult) {
+            return create_by_id(type, std::forward<Args>(args)...);
+        } else if (type == MessageType::Move || type == MessageType::AudioSource) {
+            return create_by_id_x_y(type, std::forward<Args>(args)...);
+        } else if (type == MessageType::CheckName) {
+            return create_by_id_name(type, std::forward<Args>(args)...);
+        } else {
+        }
+    }
 };
 
 #endif  // GUI_MESSAGE_H
--- src/GUI/message.cpp
+++ src/GUI/message.cpp
@@ -1,9 +1,51 @@
 #include "message.h"
 
-Message::Message(Message_type type,
+Message::MessageType Message::type() const {
+    return type_;
+}
+
+std::int64_t Message::id() const {
+    return id_;
+}
+
+std::string Message::name() const {
+    return name_;
+}
+
+double Message::x() const {
+    return x_;
+}
+
+double Message::y() const {
+    return y_;
+}
+
+Message::Message(MessageType type,
                  std::int64_t id,
                  std::string name,
                  double x,
                  double y)
-    : type(type), id(id), name(std::move(name)), x(x), y(y) {
+    : type_(type), id_(id), name_(std::move(name)), x_(x), y_(y) {
 }
+
+Message Message::create_by_id(MessageType type, std::int64_t id) {
+    return {type, id, {}, {}, {}};
+}
+
+Message Message::create_by_id_x_y(MessageType type, std::int64_t id, double x, double y) {
+    return {type, id, {}, x, y};
+}
+
+Message Message::create_by_id_name(MessageType type, std::int64_t id, std::string name) {
+    return {type, id, std::move(name), {}, {}};
+}
+
+Message Message::create_by_id_name_x_y(MessageType type,
+                                       std::int64_t id,
+                                       std::string name,
+                                       double x,
+                                       double y) {
+    return {type, id, std::move(name), x, y};
+}
+
+
