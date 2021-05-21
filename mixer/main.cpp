1a3cd7ccb9b7edd6c886442f6502336d93ec569f
TheTheoden <gromovfedor13@gmail.com>
2021-05-21 21:57:44 +0700

    MOCK

--- mixer/main.cpp
+++ mixer/main.cpp
@@ -8,36 +8,57 @@
 #include <utility>
 #include <vector>
 #include <filesystem>
+#include <chrono>
+#include <folly/io/async/NotificationQueue.h>
+/* This code will query a ntp server for the local time and display
 
-struct Element {
-private:
-    int x, y, id = -1, room_id;
+ * it.  it is intended to show how to use a NTP server as a time
+ * source for a simple network connected device.
+ * This is the C version.  The orignal was in Perl
+ *
+ * For better clock management see the offical NTP info at:
+ * http://www.eecis.udel.edu/~ntp/
+ *
+ * written by Tim Hogard (thogard@abnormal.com)
+ * Thu Sep 26 13:35:41 EAST 2002
+ * Converted to C Fri Feb 21 21:42:49 EAST 2003
+ * this code is in the public domain.
+ * it can be found here http://www.abnormal.com/~thogard/ntp/
+ *
+ */
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+
+#define NORMAL_DELAY 50
+#define NUMBER_OF_ID 5
+
+
+struct Message {
+    const double x;
+    const double y;
+    const int id;
+    const int room_id;
+    const long long time;
     AudioFile<float> data;
-public:
-    Element(int x_, int y_, int id_, int room_id_, AudioFile<float> data_) : x(x_), y(y_), id(id_), room_id(room_id_),
-                                                                             data(std::move(data_)) {};
 
-    std::pair<int, int> get_position() {
-        assert(id != -1);
-        return std::make_pair(x, y);
-    }
-
-    [[nodiscard]] int get_id() const {
-        assert(id != -1);
-        return id;
-    }
-
-    [[nodiscard]] int get_room_id() const {
-        assert(id != -1);
-        return room_id;
-    }
-
-    AudioFile<float> get_data() {
-        assert(id != -1);
-        return data;
-    }
-
+    Message(double x_, double y_, int id_, int room_id, long long time_, AudioFile<float> data_)
+            : x(x_), y(y_), id(id_), room_id(room_id), time(time_), data(std::move(data_)) {};
 };
+
+std::string add_number_to_name(const std::string &name, const int num){
+    std::stringstream ss;
+    ss << num;
+    std::string str = ss.str();
+    //std::ofstream outfile(name + str + ".wav");
+    return name + str;
+}
 
 //creates file + a path to file with name "path" + num + ".wav"
 
@@ -45,7 +66,7 @@ std::string create_path(const std::string &path, const int num) {
     std::stringstream ss;
     ss << num;
     std::string str = ss.str();
-    std::ofstream outfile(path + str + ".wav");
+    //std::ofstream outfile(path + str + ".wav");
     return path + str + ".wav";
 }
 
@@ -55,6 +76,7 @@ std::vector<AudioFile<float> > split(AudioFile<float> file, double dur) {
 
     std::vector<AudioFile<float> > splitted;
     splitted.resize(ceil(file.getLengthInSeconds() / dur));
+    //std::cout << splitted.size() << std::endl;
     for (int i = 0; i < file.getLengthInSeconds() / double(ceil(dur)); i++) {
 
         splitted[i].setAudioBufferSize(file.getNumChannels(), ceil(dur * file.getSampleRate()));
@@ -120,19 +142,174 @@ void mix(const std::string &sound_sources, const std::string &sound_path, const 
 
 AudioFile<float> join(const std::vector<AudioFile<float>> &v) {
     AudioFile<float> joint;
-    int channels = 0, len = 0;
+    int channels = 0, len = 0, numSamples = 0;
     for (const auto &c:v) {
         channels = std::max(channels, c.getNumChannels());
-        len += c.getSampleRate() * c.getNumSamplesPerChannel();
-        joint.setAudioBufferSize(channels, len);
-        for (int k = 0; k < channels; k++) {
-            for (int i = len; i < (len + c.getSampleRate() * c.getNumSamplesPerChannel()); i++) {
-                joint.samples[k][i] = c.samples[k][i - len];
+        len += c.samples[0].size();
+        numSamples += c.getNumSamplesPerChannel();
+    }
+    joint.samples.resize(channels);
+    for(int i = 0; i<channels; i++){
+        joint.samples[i].resize(len);
+    }
+    //std::cout << channels << ' ' << len << '\n';
+    len = 0;
+    for (const auto &c:v) {
+        for (int k = 0; k < c.getNumChannels(); k++) {
+            for (int i = 0; i < c.samples[k].size(); i++) {
+                joint.samples[k][i + len] = c.samples[k][i];
             }
         }
+        len+=c.samples[0].size();
     }
     return joint;
 }
 
+class QueueConsumer : public folly::NotificationQueue<Message>::Consumer {
+public:
+    QueueConsumer() = default;
+
+    void messageAvailable(Message &&value) noexcept override {
+        messages.push_back(value);
+        if (fn) {
+            fn(value);
+        }
+    }
+
+    std::function<void(Message)> fn;
+    std::deque<Message> messages;
+};
+
+long long cur_time() {
+    return std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::system_clock::now().time_since_epoch()).count();
+}
+
+double count_coef(double x1, double y1, double x2, double y2){
+    return 1.0/fmax(1.0, sqrt((x1-x2)*(x1-x2) + (y1 - y2) * (y1 - y2) ) );
+}
+
+std::vector<Message> try_to_mix(std::vector<Message>& vec){
+    std::vector<Message> result = vec;
+    for(auto &r:result){
+        for(auto v:vec){
+            if(v.id == r.id){
+                r.data.addAudioBuffer(v.data.samples, -1);
+            } else {
+                r.data.addAudioBuffer(v.data.samples, count_coef(r.x, r.y, v.x, v.y));
+                //std::cout << count_coef(r.x, r.y, v.x, v.y) << ' ';
+            }
+        }
+    }
+    return result;
+}
+
 int main() {
+    auto time_compare = [](const auto &a, const auto &b) { return a.time < b.time; };
+    std::multiset<Message, decltype(time_compare)> M[NUMBER_OF_ID];
+    AudioFile<float> emptyAudio;
+    const std::string inputFilePath = std::string(PROJECT_BINARY_DIR);
+    AudioFile<float> rick;
+    rick.load(inputFilePath + "/sound_samples/rick-astley-never-gonna-give-you-up.wav");
+    std::vector<AudioFile<float>> vec = split(rick, 0.05);
+    AudioFile<float> zhak;
+    zhak.load(inputFilePath + "/sound_samples/Zhak-Fresco-pro-bidlo.wav");
+    std::vector<AudioFile<float>> vec1 = split(zhak, 0.05);
+    //for(int i = 0; i<vec.size(); i++){
+    //vec[i].save(create_path(inputFilePath + "/splitted/output", i));
+    //}
+
+    folly::EventBase eventBase;
+    QueueConsumer consumer;
+    consumer.fn = [&](const Message &msg) {
+        if (msg.id == -1) {
+            consumer.stopConsuming();
+        }
+    };
+
+    std::vector<AudioFile<float>> example[NUMBER_OF_ID];
+
+    folly::NotificationQueue<Message> queue;
+    consumer.startConsuming(&eventBase, &queue);
+    long long cnt = 0, ticker = cur_time() + 100;
+    int counter = 0;
+    while (cnt < 500) {
+        //usleep(NORMAL_DELAY*10);
+        long long ms = cur_time();
+        queue.putMessage(Message{-50.0+static_cast<double>(cnt)/5, 0, 1, 1, ms+cnt*50, vec[cnt]});
+        queue.putMessage(Message{1, 1, 2, 1, ms+cnt*50, vec1[cnt]});
+        queue.putMessage(Message{-1, -1, -1, -1, 0, vec1[cnt]});
+        cnt++;
+        eventBase.loop();
+        while (!consumer.messages.empty()) {
+            //std::cout << consumer.messages.back().id << std::endl;
+            if (consumer.messages.back().id != -1) {
+                M[consumer.messages.back().id].insert(consumer.messages.back());
+                if (M[consumer.messages.back().id].size() == 1) {
+                    cnt++;
+                }
+            }
+            consumer.messages.pop_back();
+        }
+
+        consumer.startConsuming(&eventBase, &queue);
+        //std::cout << cur_time() << ' ' << ticker << '\n';
+        if(cur_time() >= ticker){
+            std::cout << "ticker in progress\n";
+            std::vector<Message> input;
+            for(auto & m : M){
+                while(!m.empty() && m.begin()->time < ticker - 100){
+                    std::cout << m.begin()->time << std::endl;
+                    m.erase(m.begin());
+                }
+                if(!m.empty() && m.begin()->time >= ticker - 100 && m.begin()->time < ticker - 50){
+                    input.push_back(*m.begin());
+                }
+            }
+            if(!input.empty()) {
+                std::vector<Message> output = try_to_mix(input);
+                for (int i = 0; i < output.size(); i++) {
+                    example[i].push_back(output[i].data);
+                    //std::cout << "pushed to example[" << i << "]\n";
+                    //output[i].data.save(create_path(add_number_to_name(inputFilePath + "/splitted", i) + "/output", counter));
+                }
+            }
+            ticker+=50;
+            counter++;
+        }
+    }
+    //std::cout << cur_time() << ' ' << M[1].size() << ' ' << M[2].size() << '\n';
+    while(counter < 490){
+        if(cur_time() >= ticker){
+            //std::cout << "ticker " << counter << " in progress\n";
+            std::vector<Message> input;
+            for(auto & m : M){
+                while(!m.empty() && m.begin()->time < ticker - 100){
+                    m.erase(m.begin());
+                }
+                if(!m.empty() && m.begin()->time >= ticker - 100 && m.begin()->time < ticker - 50){
+                    //std::cout << m.begin()->time << std::endl;
+                    input.push_back(*m.begin());
+                    m.erase(m.begin());
+                }
+            }
+            if(!input.empty()) {
+                std::vector<Message> output = try_to_mix(input);
+                for (int i = 0; i < output.size(); i++) {
+                    example[i].push_back(output[i].data);
+                    //std::cout << "pushed to example[" << i << "]\n";
+                    //output[i].data.save(create_path(add_number_to_name(inputFilePath + "/splitted", i) + "/output", counter));
+                }
+            }
+            ticker+=50;
+            counter++;
+        }
+    }
+    //std::cout << cur_time() << std::endl;
+    //std::cout << example[0].size() << '\n';
+    AudioFile<float> joint0 = join(example[0]);
+    AudioFile<float> joint1 = join(example[1]);
+    joint0.save(inputFilePath + "/mixed_files/output_rick.wav");
+    joint1.save(inputFilePath + "/mixed_files/output_zhak.wav");
+    return 0;
 }
