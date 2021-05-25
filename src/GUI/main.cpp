e6aca7d7f7885bb16156cc76b54486c7d1e43cc7
Alexey Pozdnyakov <pas.9250306384@gmail.com>
2021-05-25 18:01:45 +0300

    remove mock from main.cpp

--- src/GUI/main.cpp
+++ src/GUI/main.cpp
@@ -1,12 +1,8 @@
 #include <QApplication>
-#include "mock.h"
 #include "model.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-
     Model model;
-    Mock mock(&model);
-
     return QApplication::exec();
 }
