d5886aa322716abd2eeab9d32b5743048b72fa3c
Alexey Pozdnyakov <pas.9250306384@gmail.com>
2021-05-26 03:25:28 +0300

    Update serialization

--- include/Network/network_utils.h
+++ include/Network/network_utils.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/serialization/serialization.hpp>
 #include <zmq.hpp>
 #include "message.h"
 #include "server.h"
@@ -10,4 +13,4 @@ void send(zmq::socket_t &socket, Message &&msg);
 
 Message receive(zmq::socket_t &socket);
 
-Message send_and_receive(zmq::socket_t &socket, Message msg);
+Message send_and_receive(zmq::socket_t &socket, Message &&msg);
--- server/network_utils.cpp
+++ server/network_utils.cpp
@@ -1,34 +1,29 @@
 #include "network_utils.h"
-#include <boost/archive/text_iarchive.hpp>
-#include <boost/archive/text_oarchive.hpp>
-#include <boost/serialization/map.hpp>
-#include <boost/serialization/serialization.hpp>
-#include <zmq.hpp>
-#include "server.h"
 
 void send(zmq::socket_t &socket, Message &&msg) {
     std::stringstream stream;
-    boost::archive::text_oarchive serializer(stream);
-    serializer << msg;
+    {
+        boost::archive::text_oarchive serializer(stream);
+        (serializer) & (msg);
+    }
     auto serialized = stream.str();
-    zmq::message_t request(serialized.size());
-    memcpy(request.data(), serialized.data(), serialized.size());
+    zmq::message_t request(serialized.data(), serialized.size());
     socket.send(request);
 }
 
 Message receive(zmq::socket_t &socket) {
-    Message requestMessage;
-    zmq::message_t zmqResponseMessage;
-    socket.recv(&zmqResponseMessage);
-    std::string data(static_cast<char *>(zmqResponseMessage.data()),
-                     zmqResponseMessage.size());
-    std::stringstream inputStream(data);
-    boost::archive::text_iarchive deserializer(inputStream);
-    deserializer >> requestMessage;
-    return requestMessage;
+    Message request;
+    zmq::message_t response;
+    socket.recv(&response);
+    std::stringstream stream(response.to_string());
+    {
+        boost::archive::text_iarchive deserializer(stream);
+        (deserializer) & (request);
+    }
+    return request;
 }
 
-Message send_and_receive(zmq::socket_t &socket, Message msg) {
+Message send_and_receive(zmq::socket_t &socket, Message &&msg) {
     send(socket, std::move(msg));
     return receive(socket);
 }
