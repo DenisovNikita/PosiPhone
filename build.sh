f97638712056921d078779d32bfabcb9406b7206
APozdniakov <pas.9250306384@gmail.com>
2021-11-11 00:25:12 +0300

    Update README.md

---
+++ build.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+# install folly dependencies
+apt-get install \
+    git \
+    g++ \
+    cmake \
+    libboost-all-dev \
+    libevent-dev \
+    libdouble-conversion-dev \
+    libgoogle-glog-dev \
+    libgflags-dev \
+    libiberty-dev \
+    liblz4-dev \
+    liblzma-dev \
+    libsnappy-dev \
+    make \
+    zlib1g-dev \
+    binutils-dev \
+    libjemalloc-dev \
+    libssl-dev \
+    pkg-config \
+    libunwind-dev \
+    libunwind8-dev \
+    libelf-dev \
+    libdwarf-dev
+
+# install fmt
+git clone https://github.com/fmtlib/fmt.git && \
+cd fmt && \
+mkdir _build && cd _build && \
+cmake .. && \
+make -j "$(nproc)" && \
+make install && \
+cd ../..
+
+# install googletest
+wget https://github.com/google/googletest/archive/release-1.8.0.tar.gz && \
+tar zxf release-1.8.0.tar.gz && \
+rm -f release-1.8.0.tar.gz && \
+cd googletest-release-1.8.0 && \
+mkdir _build && cd _build && \
+cmake .. && \
+make -j "$(nproc)" && \
+make install && \
+cd ../..
+
+# install folly
+git clone https://github.com/facebook/folly && \
+cd folly && \
+mkdir _build && cd _build && \
+cmake .. && \
+make -j "$(nproc)" && \
+make install && \
+cd .. && \
+./build.sh
+
+# install Qt
+apt-get install \
+    qt5-default \
+    qtmultimedia5-dev \
+    libqt5multimedia5-plugins
+
+# install AudioFile
+git clone https://github.com/adamstark/AudioFile
